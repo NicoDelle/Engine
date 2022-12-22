cdc0a10beed4ef8ab959b5cab4f16c0650e5849a
NicoDelle <nicodelle04@gmail.com>
2022-12-22 22:13:03 +0100

    added scaling and fixed dict index error

--- engines/twoParticles.py
+++ engines/twoParticles.py
@@ -15,14 +15,42 @@ class SimpleInteraction(Engine):
         self.particles = dict()
         n = 0
         for item in parts.items():
-            if item[0]:
-                self.particles[n] = particle.Electron(self.surface, item[1][1], item[1][0])
+            particle_type = item[1][0]
+            speed = item[1][1]
+            position = item[1][2]
+            if particle_type:
+                self.particles[n] = particle.Electron(self.surface, position, speed)
             else:
-                self.particles[n] = particle.Proton(self.surface, item[1][1], item[1][0])
+                self.particles[n] = particle.Proton(self.surface, position, speed)
                 n += 1
+    
+    def scale_speed(self, scale_factor):
+
+        """
+        method that retrieves the fastest particle (in modulus) and then divides the speed components of each particle by the components of the speed of said particle
+        """
+
+        import math
+
+        maxSpeed = 0
+        for _, particle in self.particles.items():
+            xSpeed = particle.speed[0]
+            ySpeed = particle.speed[1]
+            if xSpeed > maxSpeed:
+                maxSpeed = xSpeed
+            if ySpeed > maxSpeed:
+                maxSpeed = ySpeed
+        
+        scaler = maxSpeed / scale_factor
+
+        for _, particle in self.particles.items():
+            scaledVx = particle.speed[0] / scaler
+            scaledVy = particle.speed[1] / scaler
+            particle.speed = (scaledVx, scaledVy)
+
 
     def run(self):
-        for key, particle in self.particles.items():
+        for _, particle in self.particles.items():
             particle.draw()
-        for key, particle in self.particles.items():
+        for _, particle in self.particles.items():
             particle.move_linear()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #Third party
 import pygame
+print("ma che cazzo")
 
 #personal
 import drawings
@@ -15,35 +16,40 @@ size = (900, 600)
 
 #<------------------------------>
 
+print("wtf?")
 pygame.init()
 surface = drawings.setSurface(size, bgColor)
 
 def main():
+    #imports
     from engines import twoParticles as engine
 
-    position = (10, 300)
+    #PROGRAM PARAMETERS
     FPS = 60
 
+    #ENGINE PARAMETERS
     n = int(input("how many particles do you need? "))
-
     partMap = dict()
     i = 0
     while i< n :
-        string = 'number {}: type 0 for proton, 1 for electron '.format(str(i + 1))
-        part = int(input(string))
-        xSpeed = int(input("what's its speed on the x axis? "))
-        ySpeed = int(input("what's its speed on the y axis? "))
-        xPosition = int(input("And what's its x coordinate? "))
-        yPosition = int(input("Finally, what's its y coordinate? "))
+        try:
+            string = 'number {}: type 0 for proton, 1 for electron '.format(str(i + 1))
+            part = int(input(string))
+            xSpeed = float(input("what's its speed on the x axis? "))
+            ySpeed = float(input("what's its speed on the y axis? "))
+            xPosition = int(input("And what's its x coordinate? "))
+            yPosition = int(input("Finally, what's its y coordinate? "))
 
-        if part == 1 or part == 0:
-            partMap[part] = [(xSpeed, ySpeed), (xPosition, yPosition)]
-            i += 1
-        else:
-            print("Bad input: retry")
-
+            if part == 1 or part == 0:
+                partMap[i] = [part, (xSpeed, ySpeed), (xPosition, yPosition)]
+                i += 1
+            else:
+                print("Bad input: retry")
+        except:
+                print("Bad input: retry")
 
     engine = engine.SimpleInteraction(surface, partMap)
+    engine.scale_speed(3)
     clock = pygame.time.Clock()
     
     run = True
